65022886d883c9c919482130f2e8344da704c189
HypMonk <41192995+HypMonk@users.noreply.github.com>
2022-07-15 18:00:26 -0400

    Player Movement & Camera Follow
    Basic Player movement using Horizontal, and jumping set to test level.

---
+++ Assets/Scripts/Game Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //Verify player was found
+        if (player == null)
+        {
+            Debug.Log("Player not found. Will Check again");
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+        else
+        {
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 3, -10);
+        }
+    }
+}
---
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 1f;
+    public float jumpVelocity = 1f;
+
+    bool _onGround = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //player movement
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0, 0) * speed * Time.deltaTime);
+
+        //player jump
+        if (Input.GetButtonDown("Jump") && _onGround)
+        {
+
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpVelocity;
+
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 _modifiedCastPosition = new Vector3(transform.position.x, transform.position.y - .3f, transform.position.z);
+        RaycastHit2D hit = Physics2D.Raycast(_modifiedCastPosition, Vector2.down, 1f);
+        if (hit.collider != null && hit.collider.tag == "Floor")
+        {
+            _onGround = true;
+            return;
+        }
+        else
+        {
+            _onGround = false;
+            return;
+        }
+
+
+    }
+}
