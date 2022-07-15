0b3d39b86c4a1202b9c0579987563961bc570188
HypMonk <41192995+HypMonk@users.noreply.github.com>
2022-07-15 18:04:46 -0400

    Better Jump
    Meant to add with previous push

---
+++ Assets/Scripts/Player Scripts/BetterJump.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetterJump : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0,10)]
+    float fallMultiplier;
+    [SerializeField]
+    [Range(0, 10)]
+    float lowJumpMultiplier;
+
+    [SerializeField]
+    Rigidbody2D rb;
+
+    private void Update()
+    {
+        if(rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        } else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+}
