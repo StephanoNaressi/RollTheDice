80a05e5ede0d36a1db217c5f466a7fa84062901f
HypMonk <41192995+HypMonk@users.noreply.github.com>
2022-07-17 13:55:42 -0400

    Button Button Script

--- Assets/Scripts/Game Scripts/Button.cs
+++ Assets/Scripts/Game Scripts/Button.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class RestartButtonController : MonoBehaviour
+public class Button : MonoBehaviour
 {
+
+    [SerializeField]
+    string _SceneName;
+
     public void OnPress()
     {
         if (GameObject.FindGameObjectWithTag("SceneManager"))
         {
-            GameObject.FindGameObjectWithTag("SceneManager").GetComponent<SwitchScene>().ChangeScene(SceneManager.GetActiveScene().name);
+            GameObject.FindGameObjectWithTag("SceneManager").GetComponent<SwitchScene>().ChangeScene(_SceneName);
         }
         else
         {
